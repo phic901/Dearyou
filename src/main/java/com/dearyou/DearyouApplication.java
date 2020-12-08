d32b4e7399a0c066aec051963432412a044ab667
e9pay <e9pay@DESKTOP-8P9R15N>
2020-12-08 11:00:53 +0900

    ..

---
+++ src/main/java/com/dearyou/DearyouApplication.java
@@ -0,0 +1,13 @@
+package com.dearyou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DearyouApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DearyouApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dearyou/DearyouApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dearyou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DearyouApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
